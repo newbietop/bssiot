7c98587fc003ecce4cb87bbe728b617115134afd
newbietop <qkrfowns45@gmail.com>
2024-11-20 15:17:09 +0900

    commit

--- rate/src/main/java/bssiot/domain/Rater.java
+++ rate/src/main/java/bssiot/domain/Rater.java
@@ -2,6 +2,7 @@ package bssiot.domain;
 
 import bssiot.RateApplication;
 import bssiot.domain.Aumoved;
+import bssiot.domain.Rated;
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
@@ -28,6 +29,9 @@ public class Rater {
     public void onPostPersist() {
         Aumoved aumoved = new Aumoved(this);
         aumoved.publishAfterCommit();
+
+        Rated rated = new Rated(this);
+        rated.publishAfterCommit();
     }
 
     public static RaterRepository repository() {
@@ -39,25 +43,12 @@ public class Rater {
 
     //<<< Clean Arch / Port Method
     public static void createRaterInfo(OrderCreated orderCreated) {
-        //implement business logic here:
-
-        /** Example 1:  new item 
         Rater rater = new Rater();
+
+        rater.setApnCd(orderCreated.getApnCd());
+        rater.setSvcContNo(orderCreated.getSvcContNo());
+
         repository().save(rater);
-
-        */
-
-        /** Example 2:  finding and process
-        
-        repository().findById(orderCreated.get???()).ifPresent(rater->{
-            
-            rater // do something
-            repository().save(rater);
-
-
-         });
-        */
-
     }
     //>>> Clean Arch / Port Method
 
